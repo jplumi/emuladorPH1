434931e27441e402b0ab64fc7edcc240856aabef
vitorcdb111 <vitordebortoli@hotmail.com>
2020-05-27 10:36:29 -0300

    leitura do arquivo

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+struct Data {
+    string addr;   
+    string value;
+};
+
+int main()
+{
+    list<Data> memory;
+    ifstream file;  
+    string fileName, fileLine;
+
+    // cout << "Input file: ";
+    // cin >> fileName;
+    // cout << endl;
+    // file.open(fileName);
+    // if(!file){
+    //     cout << "ERROR: file not found!\n";
+    //     return 1;
+    // }
+
+    file.open("example.txt");
+    int numInstructions=0;
+
+    // lê o arquivo de entrada e coloca as palavras na memory
+    while(getline(file, fileLine)){
+        if(fileLine == "") {
+            continue;
+        }
+        string addr, value;
+        addr.assign(fileLine, 0,2); //vai pegar a string da posicao inicial ate o tamanho definido;
+        value.assign(fileLine, 3,2); //vai pegar a partir da posicao 3 de tamanho 2;
+        memory.push_back({addr, value});
+    }
+
+    for(auto i : memory){
+        cout << i.addr << " " << i.value << endl;
+    }
+}
