f2c12da9b0f35b59fb438ca2474a2bb85866a9d1
jplumi <jpedro.lumi@gmail.com>
2020-06-01 15:01:38 -0300

    Adicionadas algumas instruçoes

--- main.cpp
+++ main.cpp
@@ -1,33 +1,137 @@
 #include <iostream>
-#include <list>
 #include "functions.cpp"
 
 using namespace std;
 
-struct Data {
-    int addr;   
-    int value;
-    bool modified;
-};
+// struct Data {
+//     int addr;   
+//     int value;
+//     bool modified;
+// };
 
 int main()
 {
-    list<Data> memory;
     FILE* file;
+    char fileName[256];
+    cout << "Input file: ";
+    cin >> fileName;
+    cout << endl;
 
-    file = fopen("example.txt", "r");
-    int numInstructions=0;
+    file = fopen(fileName, "r");
+    if(!file){
+        cout << "ERROR: File not found\n";
+        return 1;
+    }
 
+    // file = fopen("example.txt", "r");
+
+    int memory[256];
     // lê o arquivo de entrada e coloca as palavras na memory
     while(!feof(file)){
         int addr, value;
         fscanf(file, "%x %x", &addr, &value);
-        memory.push_back({addr, value, false});
+        memory[addr] = value;
     }
-
     fclose(file);
 
-    for(auto i : memory){
-        cout << i.addr << " " << i.value << endl;
+    int pc, ac=0, numInstructions=0;
+    for(pc=0; pc<255; pc++){
+        if(memory[pc] == 16){ // LDR
+            pc++;
+            int addr = memory[pc];
+            ac = memory[addr];
+            cout << "LDR " << hex << addr << " ; AC <- MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 32){ // STR
+            pc++;
+            int addr = memory[pc];
+            memory[addr] = ac;
+            cout << "STR " << hex << addr << " ; MEM[" << addr << "] <- AC\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 48){ // ADD
+            pc++;
+            int addr = memory[pc];
+            ac += memory[addr];
+            cout << "ADD " << hex << addr << " ; AC <- AC + MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 64){ // SUB
+            pc++;
+            int addr = memory[pc];
+            ac -= memory[addr];
+            cout << "SUB " << hex << addr << " ; AC <- AC - MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 80){ // MUL
+            pc++;
+            int addr = memory[pc];
+            ac *= memory[addr];
+            cout << "MUL " << hex << addr << " ; AC <- AC * MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 96){ // DIV
+            pc++;
+            int addr = memory[pc];
+            ac /= memory[addr];
+            cout << "DIV " << hex << addr << " ; AC <- AC / MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 112){ // NOT
+            ac = ~ac;
+            cout << "NOT    ; AC <- !AC\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 128){ // AND
+            pc++;
+            int addr = memory[pc];
+            ac = ac & memory[addr];
+            cout << "AND " << hex << addr << " ; AC <- AC & MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 144){ // OR
+            pc++;
+            int addr = memory[pc];
+            ac = ac | memory[addr];
+            cout << "OR  " << hex << addr << " ; AC <- AC | MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        if(memory[pc] == 160){ // XOR
+            pc++;
+            int addr = memory[pc];
+            ac = ac ^ memory[addr];
+            cout << "XOR " << hex << addr << " ; AC <- AC ^ MEM[" << hex << addr << "]\n";
+            numInstructions++;
+            continue;
+        }
+        // if(memory[pc] == 176){ // JMP
+        //     pc++;
+        // }
+        if(memory[pc] == 240){
+            pc++;
+            cout << "HTL\n";
+            numInstructions++;
+            break;
+        }
     }
+
+    cout << endl << numInstructions << " instructions executed\n\n";
+
+    cout << "Registers:\n"
+        << "AC " << hex << ac << endl
+        << "PC " << hex <<  pc << endl;
+
+    cout << "MEMORY: " << memory[128] << endl;
+
+    return 0;
 }
